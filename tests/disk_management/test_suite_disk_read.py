bd6887ff132d933807af01be9ca8f16d88fba48a
Jamy Bailly <jamy.bailly.dev@gmail.com>
2019-09-03 18:11:51 +0200

    Add TU - msdos with 4 primary partition

--- tests/disk_management/test_suite_disk_read.py
+++ tests/disk_management/test_suite_disk_read.py
@@ -9,4 +9,6 @@ def test_suite_disk_read():
     suite = unittest.TestSuite()
     test_case = create_disk_test_case(testcase_type=TestDiskRead, scenario_data={'vhd_name': 'gpt_512M_4p'})
     suite.addTest(loader.loadTestsFromTestCase(test_case))
+    test_case = create_disk_test_case(testcase_type=TestDiskRead, scenario_data={'vhd_name': 'msdos_512M_4primary'})
+    suite.addTest(loader.loadTestsFromTestCase(test_case))
     return suite
